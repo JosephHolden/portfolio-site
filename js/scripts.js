21e397e0258616ef439fa36e5cd7c41548694e63
JosephHolden <j_holden@hotmail.com>
2021-11-10 20:48:19 +0100

    Responsive Nav Bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 alert("Enjoy looking through my portfolio!");
+function menuToggle() {
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle') {
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
